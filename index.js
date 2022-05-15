eaf2ac1e5b9b7084d807e5a5d598dae20849d977
abx <abx@abc.com>
2022-05-15 10:48:18 +0530

    This is changed file

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits',0);
 
 app.get('/',(req,res) => {
+    
     res.send('Nunber of visits is' + visits);
     client.set('visits',parstInt(visits)+1);
 
 });
 
-app.listen(8080),() =>
+app.listen(8081,() =>
 {
     console.log('listening to port 8081');
-}
+});

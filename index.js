59257f0e280ca96961b7ce51b82986d89da88be3
abx <abx@abc.com>
2022-05-15 09:36:06 +0530

    This is first commit for lab 2

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient();
+client.set('visits',0);
+
+app.get('/',(req,res) => {
+    res.send('Nunber of visits is' + visits);
+    client.set('visits',parstInt(visits)+1);
+
+});
+
+app.listen(8080),() =>
+{
+    console.log('listening to port 8081');
+}
